83abaffcf2be9db67ffba5f637fb5b996b9197e7
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-01 17:50:35 -0500

    Added Zoom Reset and Decreased Zoom In Factor

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import tkinter as tk
 from myclass import MyTkImage
 
-def LeftClick(event):
-    img.zoom_out(2)
+def update_canvas():
     canvas.delete('background')
     canvas.image = img.return_tk()
     canvas.create_image(0,0,anchor=tk.NW,image=canvas.image)
     print(img.return_pil())
 
+def LeftClick(event):
+    img.zoom_out(2)
+    update_canvas()
+
 def RightClick(event):
-    img.zoom_in(2)
-    canvas.delete('background')
-    canvas.image = img.return_tk()
-    canvas.create_image(0,0,anchor=tk.NW,image=canvas.image)
-    print(img.return_pil())
+    img.zoom_in(1.1)
+    update_canvas()
+
+def MiddleClick(event):
+    img.zoom_reset()
+    update_canvas()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     img = MyTkImage('labels.jpeg')
     root.bind('<Button-1>', LeftClick)
+    root.bind('<Button-2>', MiddleClick)
     root.bind('<Button-3>', RightClick)
     canvas = tk.Canvas(root, width=1280, height=720)
     canvas.pack()
--- myclass.py
+++ myclass.py
@@ -4,15 +4,16 @@ from PIL import Image, ImageTk
 class MyTkImage:
 
     def __init__(self, fp: str):
+        self.fp = fp
         self.pil = Image.open(fp)
         self.tk_img = ImageTk.PhotoImage(Image.open(fp))
 
     def update_tk(self):
         self.tk_img = ImageTk.PhotoImage(self.pil)
 
-    def zoom_in(self, factor: int):
-        width = self.pil.width * factor
-        height = self.pil.height * factor
+    def zoom_in(self, factor: float):
+        width = round(float(self.pil.width) * factor)
+        height = round(float(self.pil.height) * factor)
         self.pil = self.pil.resize((width, height))
         self.update_tk()
 
@@ -20,6 +21,10 @@ class MyTkImage:
         width = self.pil.width // factor
         height = self.pil.height // factor
         self.pil = self.pil.resize((width, height))
+        self.update_tk()
+
+    def zoom_reset(self):
+        self.pil = Image.open(self.fp)
         self.update_tk()
 
     def return_pil(self):
