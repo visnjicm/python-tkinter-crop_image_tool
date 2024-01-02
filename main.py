5e5317560facba4bbc7a54b75b0df7f829b8e49d
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-01 19:11:33 -0500

    Changed keybinds a bit

--- main.py
+++ main.py
@@ -7,45 +7,46 @@ def update_canvas(x,y):
     canvas.create_image(x,y,anchor=tk.NW,image=canvas.image)
     print(img.return_pil())
 
-def LeftClick(event):
-    img.zoom_out(1.1)
-    update_canvas(img.x, img.y)
+def KeyPress(event):
+    if event.char == '-':
+        img.zoom_out(1.1)
+        update_canvas(img.x, img.y)
 
-def RightClick(event):
-    img.zoom_in(1.1)
-    update_canvas(img.x, img.y)
+    if event.char == '=':
+        img.zoom_in(1.1)
+        update_canvas(img.x, img.y)
 
-def MiddleClick(event):
-    img.zoom_reset()
-    update_canvas(img.x, img.y)
+    if event.char == '0':
+        img.zoom_reset()
+        update_canvas(img.x, img.y)
 
 def WKey(event):
-    img.y = img.y  + 5
+    img.y = img.y  + 10
     update_canvas(img.x, img.y)
 
 def AKey(event):
-    img.x = img.x - 5
+    img.x = img.x - 10
     update_canvas(img.x, img.y)
 
 def SKey(event):
-    img.y = img.y - 5
+    img.y = img.y - 10
     update_canvas(img.x, img.y)
 
 def DKey(event):
-    img.x = img.x + 5
+    img.x = img.x + 10
     update_canvas(img.x, img.y)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     img = MyTkImage('labels.jpeg')
-    root.bind('<Button-1>', LeftClick)
-    root.bind('<Button-2>', MiddleClick)
-    root.bind('<Button-3>', RightClick)
-    root.bind('<w>', WKey)
-    root.bind('<a>', AKey)
-    root.bind('<s>', SKey)
-    root.bind('<d>', DKey)
+
+    root.bind("<w>", WKey)
+    root.bind("<a>", AKey)
+    root.bind("<s>", SKey)
+    root.bind("<d>", DKey)
+
+    root.bind('<KeyPress>', KeyPress)
     canvas = tk.Canvas(root, width=1280, height=720)
     canvas.pack()
     canvas.image = img.return_tk()
