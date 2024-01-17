40d1793e574a48a7a146a4a3f5375721347cd148
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-17 10:58:44 -0500

    Enhanced Rectangle/Crop Preview

--- TkinterApp.py
+++ TkinterApp.py
@@ -12,7 +12,7 @@ class TkinterApp():
         self.canvas.create_image(self.img.x, self.img.y, anchor=tk.NW, image=self.canvas.image, tags='background')
 
     def _draw_rectangle(self):
-        self.canvas.create_rectangle(self.rect_x0, self.rect_y0, self.rect_x1, self.rect_y1)
+        self.canvas.create_rectangle(self.rect_x0, self.rect_y0, self.rect_x1, self.rect_y1, tags="rectangle")
 
     def _key_press(self, event):
         if event.char == '-':
@@ -23,6 +23,9 @@ class TkinterApp():
 
         if event.char == '0':
             self.img.zoom_reset()
+            self.canvas.delete("rectangle")
+            self.x0_y0_set = False
+            self.x1_y1_set = False
             self.img.x, self.img.y = 0, 0
 
         if event.char == 'w':
@@ -56,8 +59,17 @@ class TkinterApp():
         self._update_canvas()
 
     def _left_click(self, event):
+        self.canvas.delete("rectangle")
+        self.x0_y0_set = False
+        self.x1_y1_set = False
         self.rect_x0 = event.x
         self.rect_y0 = event.y
+        self.x0_y0_set = True
+
+    def _mouse_motion(self, event):
+        if self.x0_y0_set == True and self.x1_y1_set == False:
+            self.canvas.delete("rectangle")
+            self.canvas.create_rectangle(self.rect_x0, self.rect_y0, event.x, event.y, tags="rectangle")
 
     def _middle_click(self, event):
         print(self.rect_x0, self.rect_y0, self.rect_x1, self.rect_y1)
@@ -69,10 +81,14 @@ class TkinterApp():
         file_path = f"files/output_{timestamp}.png"
         new_img.save(fp=file_path)
         self._update_canvas()
+        self.x0_y0_set = False
+        self.x1_y1_set = False
 
     def _right_click(self, event):
         self.rect_x1 = event.x
         self.rect_y1 = event.y
+        self.x1_y1_set = True
+        self.canvas.delete("rectangle")
         self._draw_rectangle()
 
     def __init__(self):
@@ -81,6 +97,7 @@ class TkinterApp():
         self.root.bind("<Button-2>", self._middle_click)
         self.root.bind("<Button-3>", self._right_click)
         self.root.bind('<KeyPress>', self._key_press)
+        self.root.bind('<Motion>', self._mouse_motion)
 
         self.img = MyTkImage(self.image_path)
         self.canvas = tk.Canvas(self.root, width=1280, height=720)
@@ -94,3 +111,6 @@ class TkinterApp():
         self.rect_x1 = None
         self.rect_y0 = None
         self.rect_y1 = None
+
+        self.x0_y0_set = False
+        self.x1_y1_set = False
